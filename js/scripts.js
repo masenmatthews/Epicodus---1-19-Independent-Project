fe973bee903676e23a0ffb64925d84e53a79c9c6
Masen Matthews <masenmatthews@gmail.com>
2018-01-19 13:47:38 -0800

    Add + manipulate for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-var values = [""];
+var input = "";
 var inputResponse = [];
 
 $(document).ready(function() {
   $("form#submit_Form").submit(function(event) {
   event.preventDefault();
-  var inputValues = $("input#")
+  input = $("input#numericalInput").val();
+  var inputArray = input.split("");
+  for (var index=0; index < inputArray.length; index += 1) {
+
+  }
+
   });
 });
