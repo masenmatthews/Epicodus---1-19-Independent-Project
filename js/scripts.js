31a0ffcb2565c8857611db2d34c12ee9f329c87a
Masen Matthews <masenmatthews@gmail.com>
2018-01-19 09:52:45 -0800

    Establish submit button functionality in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-$(document).ready(function();
+var inputValue = "";
+
+$(document).ready(function() {
+  $("form#mainForm").submit(function(event) {
+  event.preventDefault();
+  inputValue = $("#inputNumber").val();
+
+  });
+});
