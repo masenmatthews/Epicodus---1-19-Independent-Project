88264588465ccb0cad477f99fa1f168512e464d2
Masen Matthews <masenmatthews@gmail.com>
2018-01-19 16:08:33 -0800

    continued attempts at establishing js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 var figures = "";
+var beep = ("beep!");
+var boop = ("boop!");
+var sorry = ("Sorry, Dave. I can't do that!");
+var tryagain = ("Try again!");
 
 $(document).ready(function() {
   $("form#entryForm").submit(function(event) {
   event.preventDefault();
-    figures = $("#numEntry").val();
-    var figuresArray = [figures];
-    for (var index = 0; index >= 0; index += 0) {
-
+    figures = $("input#numEntry").val();
+    var figuresArray = [];
+    figuresArray.push(figures);
+    for (var index = 0; index <= figuresArray.length; index += 0) {
+      if(figuresArray[index] === 0) {
+      figuresArray[index] = (beep);
     }
+      else if(figuresArray[index] === 1) {
+      figuresArray[index] = (boop);
+    }
+      else if(figuresArray[index] === 3) {
+      figuresArray[index] = (sorry);
+    }
+      else {
+      figuresArray[index] = (tryagain);
+    }
+  }
+  var result=figuresArray.push("");
+  $("#resultWell").text();
   });
 });
