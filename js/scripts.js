493bcd8e99f4b6ebdb2c9ee94c33b852dca7b375
Masen Matthews <masenmatthews@gmail.com>
2018-01-26 21:26:06 -0800

    continue tweaking front end + back end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 // back end
 var mainArray = [];
+
 var beepBoopArray = function() {
-for (var index = 0; index <= beepBoopArray.length; index += 1) {
-  mainArray.push(beepBoopArray);
-}
-if (beepBoopArray[index] === 0) {
-    $("#result-well").text("Beep!");
-  } else if (beepBoopArray[index] === 1) {
-    $("#result-well").text("Boop!");
-  } else if (beepBoopArray[index] === 3) {
-    $("#result-well").text("Sorry, Dave. I can't do that!");
+var i = 0;
+for (var i = 0; i <= beepBoopArray.length; i += 1) {
+  if (beepBoopArray[i] === 0) {
+    mainArray.push("Beep!");
+  } else if (beepBoopArray[i] === 1) {
+    mainArray.push("Boop!");
+  } else if (beepBoopArray[i] === 3) {
+    mainArray.push("Sorry, Dave. I can't do that!");
   } else {
     mainArray.push(beepBoopArray);
   }
   return mainArray;
+  }
 };
 
 // UI
 $(document).ready(function() {
   $("form#entry-form").submit(function(event) {
     event.preventDefault();
-    var emptyArray = $("#num-entry").val();
-    $("#result-well").text($(mainArray).val());
+    beepBoopArray();
+    var mainArray = $("#num-entry").val();
+    $("#result-well").text($("<h1>" + mainArray + "</h1>"));
   });
 });
