0f190607730e3bba289874eba329ce4b0eedf50a
Masen Matthews <masenmatthews@gmail.com>
2018-01-27 13:39:46 -0800

    further tweak js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
 // back end
 var mainArray = [];
-var beepBoop = [];
 
-var beepBoop = function(mainArray) {
-for (var index = 0; index <= mainArray.length; index += 1) {
-  if (mainArray[index] === 0) {
-    mainArray.push("Beep!");
-  } else if (mainArray[index] === 1) {
-    mainArray.push("Boop!");
-  } else if (mainArray[index] === 3) {
-    mainArray.push("Sorry, Dave. I can't do that!");
+var beepBoop = function(value) {
+  if (value === 0) {
+    return "Beep!";
+  } else if (value === 1) {
+    return "Boop!";
+  } else if (value === 3) {
+    return "Sorry, Dave. I can't do that!";
   } else {
-    mainArray.push(mainArray[index]);
+    return value;
   }
-    return mainArray;
-  }
-};
+}
 
 // UI
 $(document).ready(function() {
   $("form#entry-form").submit(function(event) {
     event.preventDefault();
-    beepBoop(mainArray);
-    var mainArray = $("#num-entry").val();
-    $("#result-well").text($(mainArray));
-    this.reset();
+    var inputValue = $("#num-entry").val();
+    for (var i = 0; i <= inputValue.length; i ++) {
+      mainArray.push(beepBoop(i));
+    }
+    $("#result-well").text(mainArray);
   });
 });
