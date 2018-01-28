2abbb674de0568cf9f910231bc8e41239f8eff9c
Masen Matthews <masenmatthews@gmail.com>
2018-01-28 15:58:48 -0800

    undo scrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 // business
+var beepBoop = function(value) {
+  var mainArray = [];
+  for (var i = 0; i <= value.length; i++) {
+  if (value === 3) {
+      mainArray.push("Sorry, Dave!");
+  } else if (value === 1) {
+      mainArray.push("Boop!");
+  } else if (value === 0) {
+      mainArray.push("beep");
+  } else {
+      return mainArray;
+    }
+  };
+};
 
 // user
 $(document).ready(function() {
   $("form#entry-form").submit(function(event) {
     event.preventDefault();
     var value = $("#num-entry").val();
+    mainArray.push(value);
+    beepBoop(mainArray);
+    $("#result-well").text(mainArray);
+    console.log(mainArray);
   });
 });
