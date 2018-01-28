d10a3e3fc8d1d2bf5c872dd1640dc239eb16eb7e
Masen Matthews <masenmatthews@gmail.com>
2018-01-28 15:53:48 -0800

    scrap js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,9 @@
 // business
-var mainArray = [];
-
-var beepBoop = function(value) {
-  if (value === 0) {
-      return "Beep!";
-  } else if (value === 1) {
-      return "Boop!";
-  } else if (value === 3) {
-      return "Sorry, Dave. I can't do that!";
-  } else {
-      return value;
-    }
-}
 
 // user
 $(document).ready(function() {
   $("form#entry-form").submit(function(event) {
     event.preventDefault();
-    var inputValue = $("#num-entry").val();
-    for (var i = 0; i < inputValue.length; i ++) {
-      mainArray.push(beepBoop(i));
-    }
-     $("#result-well").text(mainArray);
+    var value = $("#num-entry").val();
   });
 });
