81cda20466bda37e628239f61aafbd52c968b437
Masen Matthews <masenmatthews@gmail.com>
2018-01-27 20:49:29 -0800

    further js revision - tweak for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-// back end
+// business
 var mainArray = [];
 
 var beepBoop = function(value) {
   if (value === 0) {
-    return "Beep!";
+      return "Beep!";
   } else if (value === 1) {
-    return "Boop!";
+      return "Boop!";
   } else if (value === 3) {
-    return "Sorry, Dave. I can't do that!";
+      return "Sorry, Dave. I can't do that!";
   } else {
-    return value;
-  }
+      return value;
+    }
 }
 
-// UI
+// user
 $(document).ready(function() {
   $("form#entry-form").submit(function(event) {
     event.preventDefault();
     var inputValue = $("#num-entry").val();
-    for (var i = 0; i <= inputValue.length; i ++) {
+    for (var i = 0; i < inputValue.length; i ++) {
       mainArray.push(beepBoop(i));
     }
-    $("#result-well").text(mainArray);
+     $("#result-well").text(mainArray);
   });
 });
